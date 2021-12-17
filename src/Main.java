c80ea5a70abc7275426574e3cd853b00e8742ae2
Maria Bychkova <mar.byckova@gmail.com>
2021-12-18 00:43:24 +0300

    Calculate BMI

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,9 @@
 public class Main {
-
+    public static void main(String[] args) {
+        BmiService service = new BmiService();
+        int weight = 70;
+        int height = 168;
+        double BmiScore = service.calculate(weight, height);
+        System.out.printf("%.1f", BmiScore);
+    }
 }
